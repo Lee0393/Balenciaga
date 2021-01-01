5d4809a34d5dc2fe28cf28290e2230598f7b2912
lixin <li_xin@topsec.com.cn>
2021-01-01 22:33:41 +0800

    init

---
+++ src/main/java/com/vin/balenciaga/BalenciagaApplication.java
@@ -0,0 +1,13 @@
+package com.vin.balenciaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalenciagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalenciagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vin/balenciaga/BalenciagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vin.balenciaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalenciagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
